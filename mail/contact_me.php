601efe51413d5db438e6e6aa841b3fe173529624
PaulMaldonado <paul_pastore10@hotmail.com>
2022-08-12 12:37:04 -0500

    Agregando cambios a la pagina web

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -5,7 +5,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "¡No se proporcionan argumentos!";
 	return false;
    }
 	
@@ -14,11 +14,11 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$to = 'ventas@viverosquezada.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulario de contacto del sitio Web:  $name";
+$email_body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nCorreo electrónico: $email_address\n\nMensaje:\n$message";
+$headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responder a: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
